9f2d26f97314520481aa84f157eb4d99f405c462
dasrhan devani <devanidarshan333@gmail.com>
2023-07-13 17:21:46 +0530

    13-07-23

---
+++ doller.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int doller, rupees;
+    cout<<"Enter a doller:";
+    cin>>doller;
+    rupees = doller * 80;
+    cout<<"rupees is"<<rupees;
+}
---
+++ triangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int height, base, triangle;
+    cout<<"Enter a height:";
+    cin>>height;
+    cout<<"Enter a base:";
+    cin>>base;
+    triangle = (height * base)/2;
+    cout<<"area of triangle is"<<triangle;
+    
+}
